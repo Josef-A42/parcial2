d4696b4a283c4891e852a2d4ab2bea7295ab80b7
Jose Agudelo <est.josef.agudelo@unimilitar.edu.co>
2026-04-24 12:54:10 -0500

    nueva funcionalidad: implementación algoritmo de punto medio (circunferencia)

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-function plotPixel(x, y, color='#ffffff') {
+function plotPixel(x, y, color) {
+    if (color === undefined) color = "#ffffff";
     ctx.fillStyle = color;
     ctx.fillRect(x, y, 1, 1);
 }
 
+// midpoint circle algorithm
+function drawCircle(xc, yc, r) {
+    var x = 0;
+    var y = r;
+    var p = 1 - r;
+
+    while (x <= y) {
+        // draw the 8 symmetric points
+        plotPixel(xc + x, yc + y);
+        plotPixel(xc - x, yc + y);
+        plotPixel(xc + x, yc - y);
+        plotPixel(xc - x, yc - y);
+        plotPixel(xc + y, yc + x);
+        plotPixel(xc - y, yc + x);
+        plotPixel(xc + y, yc - x);
+        plotPixel(xc - y, yc - x);
+
+        x = x + 1;
+        if (p < 0) {
+            p = p + 2 * x + 1;
+        } else {
+            y = y - 1;
+            p = p + 2 * x + 1 - 2 * y;
+        }
+    }
+}
+
+// draw a test circle in the middle
+drawCircle(400, 300, 100);
+
+// function to draw the midpoint of the circumference
+
+function drawCircleMidpoint(cx, cy, r, color = '#ffffff') {
+  let x = 0;
+  let y = r;
+  let p = 1 - r; 
+  plotCirclePoints(cx, cy, x, y, color);
+
+  while (x < y) {
+    x++;
+
+    if (p < 0) {
+      
+      p = p + 2 * x + 1;
+    } else {
+      
+      y--;
+      p = p + 2 * x - 2 * y + 1;
+    }
+
+    plotCirclePoints(cx, cy, x, y, color);
+  }
+}
+
+drawCircleMidpoint(400, 300);
